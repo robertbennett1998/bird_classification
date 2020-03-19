0cbfa41b4bde40d4a5f10aa61b0e7011d8d7b6ba
Robert Bennett <robertbennett1998@outlook.com>
2020-03-19 13:04:54 +0000

    Cats and dog network performs fairly well. But lots of room for improvement, seems to be overfitting the training data.

--- bird_classification.py
+++ bird_classification.py
@@ -6,32 +6,42 @@ import hpo.strategies.genetic_algorithm as hpo_strategy_ga
 import bird_classification_data
 import os
 
+
+ray.init()
+
 data_dir = "/home/rob/dissertation/data/bird_classification"
 cache_path = os.path.join(os.getcwd(), ".cache")
 if not os.path.exists(cache_path):
     os.mkdir(cache_path)
 
-ray.init()
+
+def construct_chromosome():
+    return hpo.strategies.genetic_algorithm.DefaultChromosome(model_configuration)
+
+
+def construct_bird_data():
+    return bird_classification_data.BirdData(data_dir, cache_path, 100, 100, 100)
+
+
+###############################################################################
+# TODO: Remove this code, example only...
+# results = hpo.Results.load(os.path.join(os.getcwd(), ".tmp/hpo.results.tmp"))
+#
+# model = hpo.Hpo.build_model(results.history()[0].model_configuration())
+# model.train(construct_bird_data)
+# exit()
+###############################################################################
 
 optimiser = hpo.Optimiser(optimiser_name="optimiser_adam", optimiser_type=tf.keras.optimizers.Adam, hyperparameters=[
     hpo.Parameter(parameter_name="learning_rate", parameter_value=0.001,
                   value_range=[1 * (10 ** n) for n in range(0, -7, -1)])
 ])
 
-model_configuration = hpo.ModelConfiguration(optimiser=optimiser, layers=model_configurations.VGG_A_11, loss_function="categorical_crossentropy", number_of_epochs=10)
+model_configuration = hpo.ModelConfiguration(optimiser=optimiser, layers=model_configurations.CATS_AND_DOGS, loss_function="categorical_crossentropy", number_of_epochs=10)
 print(model_configuration.number_of_hyperparameters())
 model_configuration.hyperparameter_summary(True)
 
-
-def construct_bird_data():
-    return bird_classification_data.BirdData(data_dir, cache_path, 100, 100, 100)
-
-def construct_chromosome():
-    return hpo.strategies.genetic_algorithm.DefaultChromosome(model_configuration)
-
-construct_bird_data().load()
-
-strategy = hpo_strategy_ga.GeneticAlgorithm(population_size=30, max_iterations=8, chromosome_type=construct_chromosome,
+strategy = hpo_strategy_ga.GeneticAlgorithm(population_size=30, max_iterations=10, chromosome_type=construct_chromosome,
                                             survivour_selection_stratergy="roulette")
 
 strategy.mutation_strategy().mutation_probability(0.05)
--- bird_classification_models.py
+++ bird_classification_models.py
@@ -1,5 +1,6 @@
 import hpo
 import tensorflow as tf
+import numpy as np
 
 # https://towardsdatascience.com/vgg-neural-networks-the-next-step-after-alexnet-3f91fa9ffe2c
 VGG_A_11 = [
@@ -138,3 +139,117 @@ VGG_A_11 = [
         hyperparameters=[
         ])
 ]
+
+CATS_AND_DOGS = [
+    hpo.Layer(layer_name="input_layer_conv_2d", layer_type=tf.keras.layers.Conv2D,
+    parameters=[
+        hpo.Parameter(parameter_name="filters", parameter_value=16, value_range=[2**x for x in range(1, 9)], constraints=None),# range from 2-512
+        hpo.Parameter(parameter_name="kernel_size", parameter_value=3, value_range=range(2, 11), constraints=None),#kernal size range from 2 to 10
+        hpo.Parameter(parameter_name="padding", parameter_value="same", value_range=["same", "valid"], constraints=None),#need to add more
+        hpo.Parameter(parameter_name="activation", parameter_value="relu", value_range=["relu", "tanh", "sigmoid"], constraints=None),
+        hpo.Parameter(parameter_name="input_shape", parameter_value=(224, 224, 3))
+    ],
+    hyperparameters=[
+    ]),
+
+    hpo.Layer(layer_name="hidden_layer_1_max_pooling", layer_type=tf.keras.layers.MaxPooling2D,
+    parameters=[],
+    hyperparameters=[]),
+
+    hpo.Layer(layer_name="hidden_layer_2_conv_2d", layer_type=tf.keras.layers.Conv2D,
+    parameters=[
+        hpo.Parameter(parameter_name="filters", parameter_value=32, value_range=[2**x for x in range(1, 9)], constraints=None),# range from 2-512
+        hpo.Parameter(parameter_name="kernel_size", parameter_value=3, value_range=range(2, 11), constraints=None),#kernal size range from 2 to 10
+        hpo.Parameter(parameter_name="padding", parameter_value="same", value_range=["same", "valid"], constraints=None),#need to add more
+        hpo.Parameter(parameter_name="activation", parameter_value="relu", value_range=["relu", "tanh", "sigmoid"], constraints=None)
+    ],
+    hyperparameters=[
+    ]),
+
+    hpo.Layer(layer_name="hidden_layer_3_max_pooling", layer_type=tf.keras.layers.MaxPooling2D,
+    parameters=[],
+    hyperparameters=[]),
+
+    #4 - Conv 2D - Optimise
+    hpo.Layer(layer_name="hidden_layer_4_conv_2d", layer_type=tf.keras.layers.Conv2D,
+    parameters=[
+        hpo.Parameter(parameter_name="filters", parameter_value=64, value_range=[2**x for x in range(1, 9)], constraints=None),# range from 2-512
+        hpo.Parameter(parameter_name="kernel_size", parameter_value=3, value_range=range(2, 11), constraints=None),#kernal size range from 2 to 10
+        hpo.Parameter(parameter_name="padding", parameter_value="same", value_range=["same", "valid"], constraints=None),#need to add more
+        hpo.Parameter(parameter_name="activation", parameter_value="relu", value_range=["relu", "tanh", "sigmoid"], constraints=None)
+    ],
+    hyperparameters=[
+
+    ]),
+
+    hpo.Layer(layer_name="hidden_layer_5_max_pooling", layer_type=tf.keras.layers.MaxPooling2D,
+    parameters=[],
+    hyperparameters=[]),
+
+    #6 - Conv 2D - Optimise
+    hpo.Layer(layer_name="hidden_layer_6_conv_2d", layer_type=tf.keras.layers.Conv2D,
+    parameters=[
+        hpo.Parameter(parameter_name="filters", parameter_value=128, value_range=[2**x for x in range(1, 9)], constraints=None),# range from 2-512
+        hpo.Parameter(parameter_name="kernel_size", parameter_value=3, value_range=range(2, 11), constraints=None),#kernal size range from 2 to 11
+        hpo.Parameter(parameter_name="padding", parameter_value="same", value_range=["same", "valid"], constraints=None),#need to add more
+        hpo.Parameter(parameter_name="activation", parameter_value="relu", value_range=["relu", "tanh", "sigmoid"], constraints=None)#need to add more
+    ],
+    hyperparameters=[
+    ]),
+
+    hpo.Layer(layer_name="hidden_layer_7_max_pooling", layer_type=tf.keras.layers.MaxPooling2D,
+    parameters=[],
+    hyperparameters=[
+    ]),
+
+    hpo.Layer(layer_name="hidden_layer_8_flatten", layer_type=tf.keras.layers.Flatten,
+    parameters=[],
+    hyperparameters=[]),
+
+    hpo.Layer(layer_name="hidden_layer_9_dropout", layer_type=tf.keras.layers.Dropout,
+    parameters=[
+        hpo.Parameter(parameter_name="seed", parameter_value=42)
+    ],
+    hyperparameters=[
+        hpo.Parameter(parameter_name="rate", parameter_value=0.2, value_range=np.arange(0.0, 0.4, 0.1).tolist(), constraints=None)
+    ]),
+
+    hpo.Layer(layer_name="hidden_layer_10_dense", layer_type=tf.keras.layers.Dense,
+    parameters=[
+        hpo.Parameter(parameter_name="activation", parameter_value="relu", value_range=["relu", "tanh", "sigmoid"], constraints=None)#need to add more
+    ],
+    hyperparameters=[
+        hpo.Parameter(parameter_name="units", parameter_value=512, value_range=[2**x for x in range(7, 11)], constraints=None),#range between 4 and 512
+    ]),
+
+    hpo.Layer(layer_name="hidden_layer_11_dropout", layer_type=tf.keras.layers.Dropout,
+    parameters=[
+        hpo.Parameter(parameter_name="seed", parameter_value=42)
+    ],
+    hyperparameters=[
+        hpo.Parameter(parameter_name="rate", parameter_value=0.2, value_range=np.arange(0.0, 0.4, 0.1).tolist(), constraints=None)
+    ]),
+
+    hpo.Layer(layer_name="hidden_layer_12_dense", layer_type=tf.keras.layers.Dense,
+    parameters=[
+        hpo.Parameter(parameter_name="activation", parameter_value="relu", value_range=["relu", "tanh", "sigmoid"], constraints=None)#need to add more
+    ],
+    hyperparameters=[
+        hpo.Parameter(parameter_name="units", parameter_value=256, value_range=[2**x for x in range(6, 10)], constraints=None),#range between 4 and 512
+    ]),
+
+    hpo.Layer(layer_name="hidden_layer_12_dropout", layer_type=tf.keras.layers.Dropout,
+    parameters=[
+        hpo.Parameter(parameter_name="seed", parameter_value=42)
+    ],
+    hyperparameters=[
+        hpo.Parameter(parameter_name="rate", parameter_value=0.2 , value_range=np.arange(0.0, 0.4, 0.1).tolist(), constraints=None)
+    ]),
+
+    hpo.Layer(layer_name="output_layer_dense", layer_type=tf.keras.layers.Dense,
+    parameters=[
+        hpo.Parameter(parameter_name="activation", parameter_value="softmax", value_range=["tanh", "softmax"], constraints=None),#need to add more
+        hpo.Parameter(parameter_name="units", parameter_value=150)
+    ],
+    hyperparameters=[
+    ])]
